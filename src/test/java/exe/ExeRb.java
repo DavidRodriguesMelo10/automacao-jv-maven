3175eebcea2f89b59b23bbc600b03b5351a2e8ef
david.rmelo <david.13rodrigues@hotmail.com>
2023-01-24 11:00:16 -0300

    v0.1

---
+++ src/test/java/exe/ExeRb.java
@@ -0,0 +1,16 @@
+package exe;
+
+import br.com.davidrmelo.autoJv.Robodesc;
+
+public class ExeRb {
+
+    public static void main(String[] args){
+        Robodesc robodesc = new Robodesc ();
+        robodesc.abrirChrome();
+
+
+
+
+    }
+
+}
