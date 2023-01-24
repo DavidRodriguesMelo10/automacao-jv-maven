8286189958dcb247ec0bcbc85f9519e442094091
david.rmelo <david.13rodrigues@hotmail.com>
2023-01-24 11:00:16 -0300

    v0.1

---
+++ src/test/java/br/com/davidrmelo/autoJv/Robodesc.java
@@ -0,0 +1,18 @@
+package br.com.davidrmelo.autoJv;
+
+public class Robodesc {
+
+    public void abrirChrome(){
+        System.out.println("Estou abrindo o Chrome!");
+    }
+
+    public void fazerLogin (){
+        System.out.println("Vou fazer login!");
+
+    }
+
+    public void lançarNota (){
+        System.out.println("Vou lancar uma nota!");
+
+    }
+}
